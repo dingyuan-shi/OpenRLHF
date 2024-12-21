3fc6db4ea058808f9dbf62c42ca11b822211e787
dingyuan.shi <dingyuan.shi@outlook.com>
2024-12-21 19:37:00 +0800

    fix load ckpt bug of train prm_linearhead

--- openrlhf/cli/train_prm_linearhead.py
+++ openrlhf/cli/train_prm_linearhead.py
@@ -96,7 +96,7 @@ def train(args):
     # load checkpoint
     consumed_samples = 0
     if args.load_checkpoint and os.path.exists(args.ckpt_path):
-        _, states = strategy.load_ckpt(model.model, args.ckpt_path)
+        _, states = strategy.load_ckpt(model, args.ckpt_path)
         consumed_samples = states["consumed_samples"]
         strategy.print(f"Loaded the checkpoint: {args.ckpt_path}, consumed_samples: {consumed_samples}")
 
